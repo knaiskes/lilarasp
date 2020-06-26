74834d3b8768d19691103ea1c62d9e90d3c0d79d
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-06-26 17:25:40 +0300

    import raspberry's gpio module and set it up

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import RPi.GPIO as GPIO
 from lila_api_call import isOnline
+
+PIN = 2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarning(False)
+GPIO.setup(PIN, GPIO.OUT)
 
 def main():
     print(isOnline())
