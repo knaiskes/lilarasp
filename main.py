7344502e6a70ec9e0c169154a39f4774ac643c77
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-06-27 14:38:15 +0300

    Add async call and set led to HIGH or LOW

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import asyncio
 from lila_api_call import isOnline
 
 PIN = 2
@@ -7,9 +8,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarning(False)
 GPIO.setup(PIN, GPIO.OUT)
 
-def main():
-    print(isOnline())
+# From: https://lichess.org/api#tag/Users
+# "This API is very fast and cheap on lichess side. So you can call it quite
+# "often (like once every 5 seconds)."
 
+# 10 seconds is more than enough for my case but you can change it down to 5
+SLEEP_TIME = 10
+
+async def main():
+    while True:
+        if isOnline():
+            GPIO.output(PIN, GPIO.HIGH)
+        else:
+            GPIO.output(PIN, GPIO.LOW)
+
+        await asyncio.sleep(SLEEP_TIME)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
