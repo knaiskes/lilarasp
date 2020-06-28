8f31ed3b15d10cc396bb9d21a88a27a7feab9492
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-06-28 12:11:08 +0300

    Add keyboard interrupt exception to turn led off

--- main.py
+++ main.py
@@ -25,4 +25,7 @@ async def main():
         await asyncio.sleep(SLEEP_TIME)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        GPIO.output(PIN, GPIO.LOW)
